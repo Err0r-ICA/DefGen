3f563393d67548524722d9cb9462a53cbccd0dc2
*ERR0R* <59941931+Err0r-ICA@users.noreply.github.com>
2021-12-31 03:59:37 +0100

    Add files via upload

---
+++ deface.py
@@ -0,0 +1,235 @@
+# -*- coding: utf-8 -*-
+import os,time
+os.system('clear')
+os.system('touch deface.html')
+def banner ():
+  print '''\033[91m
+
+'''
+
+  print ('\033[38;5;198m꧁꧂'*27)
+  print ''
+  print ' \033[96m៚Author   \033[90m -->>> \033[91m ☣ \033[38;5;128mErr0r_HB'
+  print ''
+  print ' \033[97m៚Tool     \033[90m -->>> \033[91m ☣ \033[38;5;048mDeface Page Generator'
+  print ''
+  print ' \033[96m៚Telegram \033[90m -->>> \033[91m ☣ \033[38;5;128mhttps://t.me/kalit3rmux/'
+  print ''
+  print ' \033[97m៚Instagram\033[90m -->>> \033[91m ☣ \033[38;5;048m@termux_hacking'
+  print ''
+  print ' \033[96m៚Github   \033[90m -->>> \033[91m ☣ \033[38;5;128mhttps://www.github.com/Err0r-ICA/'
+  print ''
+  print ' \033[97m៚Coded By \033[90m -->>> \033[91m ☣ \033[38;5;048mHackboyz Team'
+  print ''
+  print ('\033[38;5;198m꧁꧂'*27)
+  print ''
+  print ''
+  print ''
+banner ()
+# input
+input2=raw_input('\033[93m Enter Your Hacker Name :\033[92m ') #code Name
+input3=raw_input('\033[93m Enter Logo Link : \033[92m') # logo 
+input4=raw_input('\033[93m Enter Your Team Name :\033[92m ') #Team name
+part1='''<!doctype html>
+<html lang="en">
+  <head>
+   <center> <p><font face="Jolly Lodger" color="lime" size="6">'''
+part2=input2+'<font color="#00BFFF">'+' Was Here !!!'
+part33='''</p>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.75, shrink-to-fit=no">
+    <meta name="theme-color" content="#000">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Almendra+Display|Almendra+SC|Piedra&display=swap" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Jolly+Lodger' rel='stylesheet' type='text/css'>
+    	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+			<script type="text/javascript" src="js/jquery.js"></script>
+			<script type="text/javascript" src="js/jquery-1.10.2.min.js.download"></script>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <title> Hacked By '''
+part333=input2+'''</title>
+     <iframe frameborder="0" height="1" width="1" loop="true" allow="autoplay" src="http://upir.ir/images/twzc4383c491h9jq25l.mp3"></iframe> 
+    <style>
+    	body {
+    		background-color: black;
+    		padding:0;
+    		margin:0;
+    	}
+    	.tmp {
+    		padding-top: 25%;
+    	}
+    	.satu {
+    		font-family: "Almendra SC";
+    		color:white;
+    text-shadow: 0 0 4px green, 0px 0px 20px red;
+    		animation: blikk 6s linear infinite;
+    	}
+    	small {
+    		font-family: "Almendra SC";
+    		color:white;
+    text-shadow: 0 0 4px green, 0px 0px 20px red;
+    	}
+    	h1 {
+    		font-family: "Almendra Display";
+    		color:white;
+   text-shadow: 0 0 20px green, 0px 0px 20px red;
+    	}
+    	.ico {
+    		width:250px;
+    		height:250px;
+    		margin:0;
+    		padding:0;
+    	}
+    	#particles-js {
+    		width: 100%;
+    		height: 100%;
+    		background-size: cover;
+    		background-position: 50% 50%;
+    		position: fixed;
+    		top: 0px;
+    		z-index:1;
+    	}
+    	@keyframes blikk{
+    		0% {
+    			color: #7FFFD4;
+    			text-shadow: 0 0 20px red, 0px 0px 20px green;
+    		}
+    		50% {
+    			color: #0909FF;
+    			text-shadow: 0 0 20px red, 0px 0px 20px green;
+    		}
+    		100% {
+    			color: #16E2F5;
+    			text-shadow: 0 0 20px red, 0px 0px 20px green;
+    		}
+    	}
+    </style>
+  </head>
+  <body id="">
+    <div class="tmp" id="">
+    	<div class="container">
+<br><br><center><h1><font face="Jolly Lodger" color="springgreen" size="35">Hacked By <font color="lime">'''
+part3= part33+part333
+part4=input2
+part5= '''<br></font></center>
+    	<center>'''
+    	
+    	
+part666='''<center><img src="'''
+part66='''" height='275px' width='275px'></font></center><center>'''
+part6=part666+input3+part66
+part7='''</font></font>
+<br>
+<center><font size="5" color="red">{<font color="#FF5F1F">DEAR ADMIN<font color="red">}<font size="5" color="white"></font></font>
+<br>
+<font size="4" color="#FFFF33">We Found <font color='tomato'>Vulnerability <font color='#FFFF33'>In Your <font></font><font color="white" size="4">Website...!</font></font>
+<br>
+<font size="3" color="salmon">This Is Your Just a <font color='tomato'>Warning!!!<font size="5" color="white"></font></font>
+<br>
+<font size="3" color="#FC6C85">So Fix Your Website <font color='tomato'>Vulnerability<font size="5" color="white"></font></font>
+<br>
+<center><font color="#E30B5D" size="4">And Next Time Be Careful </font></font>
+<br>
+<br>
+<br>
+<font face="Jolly Lodger" color="white" size="25">&#10026 <font color="#FD349C">TEAM <font color='white'>&#10026;  <br><font face="Jolly Lodger" color="white" size="20">'''
+part8=' <font color="white">&#9760 <font color="#FD349C">'+input4+ ' <font color="white">&#9760; '
+#  all parts
+part9='''</font></font>
+ <div id="particles-js">
+ 	<canvas class="particles-js-canvas-el" style="width: 100%; height: 100%;" width="1365" height="949"></canvas>
+</div>
+  </div>
+  <script src="https://cdn.rawgit.com/FicriPebriyana/efek/0a935a6c/efek%20salju.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    	$.getScript("https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js", function(){ 
+    	particlesJS('particles-js', {
+    		"particles": {
+    				"number": {
+    						"value": 80, 
+    						"density": {
+    						"enable": true, 
+    						"value_area": 800 
+    					} }, "color": {
+    						"value": "#FFFFFF"}, 
+    						"shape": { "type": "circle", 
+    						"stroke": { "width": 0, "color": "#000000" }, 
+    						"polygon": { "nb_sides": 5 }, 
+    						"image": { "width": 100, "height": 100 } }, 
+    						"opacity": { "value": 0.5, "random": false, 
+    						"anim": { 
+    								"enable": false, 
+    								"speed": 1, 
+    								"opacity_min": 0.1, 
+    								"sync": false } }, 
+    						"size": { 
+    								"value": 5, 
+    								"random": true, 
+    						"anim": { 
+    								"enable": false, 
+    								"speed": 40, 
+    								"size_min": 0.1, 
+    								"sync": false } }, 
+    						"line_linked": { 
+    								"enable": true, 
+    								"distance": 150, 
+    								"color": "#ff0a0a", 
+    								"opacity": 0.4, 
+    								"width": 1 }, 
+    						"move": { 
+    								"enable": true, 
+    								"speed": 2, 
+    								"direction": "none", 
+    								"random": false, 
+    								"straight": false, 
+    								"out_mode": "out", 
+    						"attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, 
+    						"interactivity": { 
+    						"detect_on": "canvas", 
+    						"events": { 
+    						"onhover": { 
+    						"enable": true, "mode": "repulse" }, 
+    						"onclick": { 
+    								"enable": true, 
+    								"mode": "push" }, 
+    						"resize": true }, 
+    						"modes": { 
+    						"grab": { "distance": 400, "line_linked": { 
+    						"opacity": 1 } }, 
+    						"bubble": { "distance": 400, "size": 50, "duration": 2, "opacity": 8, "speed": 1 }, 
+    						"repulse": { "distance": 200 }, 
+    						"push": { "particles_nb": 4 }, "remove": { 
+    						"particles_nb": 2 } } }, 
+    						"retina_detect": true, "config_demo": { 
+    						"hide_card": false, 
+    						"background_color": "#ff0a0a", 
+    						"background_image": "", 
+    						"background_position": "50% 50%", 
+    						"background_repeat": 
+    						"no-repeat", 
+    						"background_size": "cover" 
+    						} 
+    					});
+    				}); 
+    		</script>
+  </body>
+</html>'''
+parts=part1+part2+part3+part4+part5+part6+part7+part8+part9
+
+# main 
+
+biva=open ('deface.html','w')
+biva.write(parts)
+biva.close()
+os.system('mv deface.html -t $HOME/storage/downloads')
+print ''
+print ''
+print 'Deface Page Generated... Check Downloads Folder in Internal Storage'
+print ''
+time.sleep(1.0)
+print "Thanks For Using This Script. Don't Forget the Star ;)"
+print ''
+time.sleep(2.2)
+
